bed61de006ac255a22746990e942966e920ac4a3
Kacper Kornacki <60293257+repcus@users.noreply.github.com>
2021-06-12 19:52:13 +0200

    added gamemodes

--- classes.js
+++ classes.js
@@ -46,7 +46,6 @@ class Platform{
 
 class Ball{
     isMovingRight = Math.random() < 0.5;
-    isMovingDown = true;
     constructor(radius, color, speed, x, y){
         this.radius = radius;
         this.position = new Vector(x, y);
@@ -63,31 +62,58 @@ class Ball{
     }
     newPos(){
         let gameContext = myGameArea.context;
-        if (this.position.x >= gameContext.canvas.width || this.position.x <= 0){
+        if (this.position.x + this.radius >= gameContext.canvas.width || this.position.x - this.radius <= 0){
             this.velocity = new Vector(-this.velocity.x, this.velocity.y);
         }
 
-        if (this.position.y >= gameContext.canvas.height){
+        if (this.position.y + this.radius >= 0){
             this.velocity = new Vector(this.velocity.x, -this.velocity.y);
         }
         this.position = this.position.add(this.velocity);
+    }
+    ballBallCollision(ball){
+        const distance =this.position.add(this.velocity)
+            .subtract(ball.position.add(ball.velocity))
+            .magnitude;
+
+        function collisionVector(b1, b2){
+            return b1.velocity.subtract(b1.position.subtract(b2.position).multiply(b1.velocity.subtract(b2.velocity).dotProduct(b1.position.subtract(b2.position))/ b1.position.subtract(b2.position).magnitude ** 2).multiply((2 * b2.sphereArea)  / (b1.sphereArea + b2.sphereArea)));
+        }
+
+        if (distance <= this.radius + ball.radius) {
+            const v1 = collisionVector(this, ball);
+            const v2 = collisionVector(ball, this);
+            this.velocity = v1;
+            return v2;
+        }
+    }
+    get sphereArea() {
+        return 4 * Math.PI * this.radius ** 2;
     }
 }
 
 class Block{
     isColWPlatform = false;
     color = colorArr[Math.floor(Math.random() * 2)];
-
     constructor(x, y, width, height){
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.blockTimer = 0;
     }
     update(){
         let gameContext = myGameArea.context;
         gameContext.fillStyle = this.color;
         gameContext.fillRect(this.x, this.y, this.width, this.height);
+        this.blockTimer++;
+    }
+    newPos(){
+        console.log(this.blockTimer)
+        if(Math.floor(this.blockTimer ) === 500){
+            this.y += this.height;
+            this.blockTimer = 0;
+        }
     }
 }
 
@@ -101,7 +127,7 @@ class Vector{
         return new Vector(this.x + vector.x, this.y + vector.y);
     }
 
-    subtrvector2t(vector){
+    subtract(vector){
         return new Vector(this.x - vector.x, this.y - vector.y);
     }
 
--- game.js
+++ game.js
@@ -9,7 +9,7 @@
     let platformHorizontal;
     let platformVertical = null;
     let speed = 4/500 * canvasHeight;
-    let ballSpeed = speed - 1;
+    let ballSpeed = speed - 2;
     let isGamePaused = true;
     let isGameEnded = false;
     const rows = 3;
@@ -31,25 +31,29 @@
     let platformVerH = canvasWidth / 5;
     let time = 0;
     let poppedBlocks;
-    let balls = new Array();
+    let balls = [];
     let ballsCount = 1;
-
+    let gameMode;
     function startGame() {
+        gameMode = "falling";
         time = 0;
         //źródło mobile checkera http://detectmobilebrowsers.com/
         window.mobileCheck = function() {
             let check = false;
-            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+            (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series([46])0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br([ev])w|bumb|bw-([nu])|c55\/|capi|ccwa|cdm-|cell|chtm|cldc|cmd-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc-s|devi|dica|dmob|do([cp])o|ds(12|-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly([-_])|g1 u|g560|gene|gf-5|g-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd-([mpt])|hei-|hi(pt|ta)|hp( i|ip)|hs-c|ht(c([- _agpst])|tp)|hu(aw|tc)|i-(20|go|ma)|i230|iac([ \-\/])|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja([tv])a|jbro|jemu|jigs|kddi|keji|kgt([ \/])|klon|kpt |kwc-|kyo([ck])|le(no|xi)|lg( g|\/([klu])|50|54|-[a-w])|libw|lynx|m1-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t([- ov])|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30([02])|n50([025])|n7(0([01])|10)|ne(([cm])-|on|tf|wf|wg|wt)|nok([6i])|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan([adt])|pdxg|pg(13|-([1-8]|c))|phil|pire|pl(ay|uc)|pn-2|po(ck|rt|se)|prox|psio|pt-g|qa-a|qc(07|12|21|32|60|-[2-7]|i-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h-|oo|p-)|sdk\/|se(c([-01])|47|mc|nd|ri)|sgh-|shar|sie([-m])|sk-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h-|v-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl-|tdg-|tel([im])|tim-|t-mo|to(pl|sh)|ts(70|m-|m3|m5)|tx-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c([- ])|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas-|your|zeto|zte-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
             return check;
         };
         platformHorizontal = null;
         platformVertical = null;
         poppedBlocks = null;
+        poppedBlocks= [];
         platformHorizontal = new Platform(platformHorW, platformHorH, "magenta", platformHorX, platformHorY, speed);
         if(askIfVertical()){
             platformVertical = new Platform(platformVerW, platformVerH, "cyan", platformVerX, platformVerY, speed);
         }
-        poppedBlocks= new Array();
+        if(checkGameMode()){
+            gameMode = "endless";
+        }
         balls.push(new Ball(ballRadius, "black", ballSpeed, ballX, ballY));
         if(ballsCount !== 1){
             ballsCount = 1;
@@ -146,10 +150,15 @@
     }
 
     function updateGameArea() {
-        if(scoreB === 5 && ballsCount < 2){
+        if(scoreB === 5){
             balls.push(new Ball(ballRadius, "black", ballSpeed, ballX, ballY));
             scoreB = 0;
             ballsCount++;
+        }
+        for(let i = 0; i < ballsCount; i++){
+            for(let j = i + 1; j < i; j++){
+                balls[j].velocity = balls[i].ballBallCollision(balls[j]);
+            }
         }
         if(platformHorizontal.x + platformHorizontal.width > myGameArea.canvas.width){
             platformHorizontal.x = myGameArea.canvas.width - platformHorizontal.width;
@@ -167,7 +176,9 @@
 
         let gameContext = myGameArea.context;
         if (!isGamePaused) {
-            spawnBlock(poppedBlocks, blockArr, time);
+            if(gameMode === "endless"){
+                spawnBlock(poppedBlocks, blockArr, time);
+            }
             time += 20;
             document.getElementById("timer").innerHTML = Math.floor(time / 1000) + " s";
             if (isGameEnded) {
@@ -229,6 +240,9 @@
             }
             for(let i = 0; i < blockArr.length; i++){
                 blockArr[i].update();
+                if(gameMode === "falling"){
+                    blockArr[i].newPos(time);
+                }
                 if(blockArr[i].isColWPlatform && platformVertical){
                     platformBlockCollision(platformVertical, blockArr[i]);
                 }
@@ -268,7 +282,7 @@
                 }
             }
         }else{
-            myGameArea.context.font = (myGameArea.context.canvas.width/20).toString() + "px Helvetica";
+            myGameArea.context.font = (myGameArea.context.canvas.width / 20).toString() + "px Helvetica";
             myGameArea.context.textAlign = "center";
             myGameArea.context.fillText("To play, press start!", gameContext.canvas.height/2, gameContext.canvas.height/2);
         }
@@ -276,7 +290,7 @@
 
     function updateControlArena(){
         let gameContext = myControlArea.context;
-        myControlArea.clear();
+        gameContext.clear();
     }
 
     function randomNum(min, max) {
@@ -285,6 +299,10 @@
 
     function askIfVertical(){
         return confirm("Click OK if you want to have a second, vertical platform!");
+    }
+
+    function checkGameMode(){
+        return confirm("Click OK -> endless, cancel -> falling blocks");
     }
 
     function pause() {
