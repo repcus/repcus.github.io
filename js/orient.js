fc5136bd0642bcdf614ee02c4fdfb059b59dfb08
repcus <kacper.kornacki@gmail.com>
2021-08-04 23:59:07 +0200

    resp landscape

---
+++ js/orient.js
@@ -0,0 +1,19 @@
+let orientation = (screen.orientation || {}).type || screen.mozOrientation || screen.msOrientation;
+console.log(orientation);
+if (orientation === 'landscape-primary' || orientation === 'landscape-secondary') {
+    console.log("That looks good.");
+} else if (orientation === 'portrait-primary' || orientation === 'portrait-secondary') {
+    document.getElementById("pic1").src = './img/OBR.svg';
+    document.getElementById("pic2").src = './img/OBR.svg';
+}
+window.onorientationchange = function(event) {
+    orien = event.target.screen.orientation.angle;
+    console.log(orien)
+    if (orien === 0 || orien === 180) {
+        document.getElementById("pic1").src = './img/OBR.svg';
+        document.getElementById("pic2").src = './img/OBR.svg';
+    } else if (orien === 90 || orien === 270) {
+        document.getElementById("pic1").src = './img/P1.PNG';
+        document.getElementById("pic2").src = './img/P2.PNG';
+    }
+};
