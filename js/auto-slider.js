f7d56cbdeee433c6593f60a94ccee7db9c6f8b28
Kacper Kornacki <60293257+repcus@users.noreply.github.com>
2021-10-17 20:52:45 +0200

    Delete js directory

--- js/auto-slider.js
+++
@@ -1,8 +0,0 @@
-
-window.onload= ()=>{
-    sections = document.querySelectorAll(".section");
-    document.addEventListener('scroll', ()=>{
-        console.log(sections[1])
-        sections[1].scrollIntoView({behavior: "smooth", block: "start"});
-    })
-}
