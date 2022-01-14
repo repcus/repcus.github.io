f4eed25e69d6752d0a8718abe414211ab5f8a1a3
Kacper Kornacki <60293257+repcus@users.noreply.github.com>
2022-01-14 17:47:16 +0100

    Delete formtry directory

--- formtry/first.js
+++
@@ -1,8 +0,0 @@
-function go() {
-    if (document.getElementById("student").value != "" && document.getElementById("teacher").value != "") {
-        document.getElementById("questions").style.display = "block"
-        document.getElementById("first").style.display = "none"
-    } else {
-        alert("Przed rozpoczęciem testu, należy podać dane!")
-    }
-}
