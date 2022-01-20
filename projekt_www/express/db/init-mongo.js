e53142bc5ae457887584f5f1dbea48a4183d0e72
mbarancewicz <44418145+mbarancewicz@users.noreply.github.com>
2022-01-20 01:56:51 +0100

    Added docker-compose file with mongodb and init script for database, updated reademe

---
+++ projekt_www/express/db/init-mongo.js
@@ -0,0 +1,68 @@
+const { MongoClient } = require('mongodb');
+
+//todo maybe extract it to properties file
+const containerIp = '192.168.99.100';
+const mongoConnectionString = `mongodb://${containerIp}:27017`;
+const userName = "user";
+const password = "password";
+const dbName = "applicationDb";
+const operationsCollectionName = "operations";
+const recipesCollectionName = "recipes";
+
+MongoClient.connect(mongoConnectionString, { 
+  auth:{authdb:'admin', username:'root', password:'password'},
+  authSource:'admin',
+  useNewUrlParser: true }, async function(err, client) {
+
+    if(err) {
+        return console.log('Error: could not connect to mongodb')
+    }
+    console.log("Connected to mongodb");
+
+    const db = client.db(dbName);  
+    createCollection(db, operationsCollectionName);
+    createCollection(db, recipesCollectionName);
+    await db.command({
+        createRole: 'application',
+        privileges: [
+            { resource: { db: dbName, collection: operationsCollectionName }, actions:['find'] },
+            { resource: { db: dbName, collection: recipesCollectionName   }, actions:['find', 'insert'] }
+        ], roles: [] });
+    console.log("Created application role");
+    await addUserToDb(db, dbName, 'application');
+
+    await populateDb(db);
+    console.log("Populated database");
+    client.close();
+  });
+
+async function addUserToDb(db, dbName, role) {
+    await db.addUser(userName,  password, {
+        roles: [{
+            role:   role,
+            db:     dbName
+            }]
+        }, 
+        function (err, result) {
+            if (err){
+                console.log(`Error: could not add user ${userName} to database ${dbName} with role ${role}`);
+                throw err;
+            }
+            console.log(`Added user ${userName} to database ${dbName} with role ${role}`);
+        }
+    );
+}
+
+function createCollection(db, collectionName)  {
+    db.createCollection(collectionName, function(err, res) {
+        if(err) {
+            console.error(`Could not create collection ${collectionName}`);
+            throw err;
+        }
+        console.log(`Collection ${collectionName} created!`);
+    });
+}
+
+async function populateDb(db) {
+    await db.collection(operationsCollectionName).insertOne({operation: 'slice', machineOperation: 'slice'} );
+}
