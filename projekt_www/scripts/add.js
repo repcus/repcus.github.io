84c0acd32c0ab0d7cf4fa7212c37e15c6f84768f
Kacper Kornacki <60293257+repcus@users.noreply.github.com>
2022-01-30 13:51:38 +0100

    Update add.js

--- projekt_www/scripts/add.js
+++ projekt_www/scripts/add.js
@@ -317,15 +317,15 @@ function download(filename, data) {
     console.log(data)
     xhr.send(JSON.stringify(JSON.parse(data)))
     //koniec
-    var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
-    pom.setAttribute('download', filename);
+    // var pom = document.createElement('a');
+    // pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
+    // pom.setAttribute('download', filename);
 
-    if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
-    } else {
-        pom.click();
-    }
+    // if (document.createEvent) {
+    //     var event = document.createEvent('MouseEvents');
+    //     event.initEvent('click', true, true);
+    //     pom.dispatchEvent(event);
+    // } else {
+    //     pom.click();
+    // }
 }
